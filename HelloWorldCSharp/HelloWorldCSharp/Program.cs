826fea5d84760165da6d55eb166707f84844c11f
John Chavez <jchavez6521@gmail.com>
2013-03-13 21:33:37 -0500

    Change test

--- HelloWorldCSharp/HelloWorldCSharp/Program.cs
+++ HelloWorldCSharp/HelloWorldCSharp/Program.cs
@@ -18,7 +18,7 @@ namespace HelloWorldCSharp
         static void Main(string[] args)
         {
             Test t = new Test();
-            Console.WriteLine("Hello World");
+            Console.WriteLine("Hello Worldo");
             Console.WriteLine(t.value);
             Console.ReadLine();
             return;
