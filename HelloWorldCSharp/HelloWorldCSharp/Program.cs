79ae16f5ae79a463c2251eaa522ce0e07122a30d
John Chavez <jchavez6521@gmail.com>
2013-03-13 21:49:09 -0500

    Testing testBranch I think

--- HelloWorldCSharp/HelloWorldCSharp/Program.cs
+++ HelloWorldCSharp/HelloWorldCSharp/Program.cs
@@ -18,7 +18,7 @@ namespace HelloWorldCSharp
         static void Main(string[] args)
         {
             Test t = new Test();
-            Console.WriteLine("Hello Worldo");
+            Console.WriteLine("Testing my branch stuff one more time...");
             Console.WriteLine(t.value);
             Console.ReadLine();
             return;
