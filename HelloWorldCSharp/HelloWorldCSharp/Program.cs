333fa1cf4c5c88ebd44e1070d47a92fb2e8413b8
John Chavez <jchavez6521@gmail.com>
2013-03-13 21:31:54 -0500

    Initial Commit

---
+++ HelloWorldCSharp/HelloWorldCSharp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldCSharp
+{
+
+    class Test
+    {
+        public int value=5;
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test t = new Test();
+            Console.WriteLine("Hello World");
+            Console.WriteLine(t.value);
+            Console.ReadLine();
+            return;
+        }
+    }
+}
